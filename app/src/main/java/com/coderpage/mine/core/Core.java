436171556370feb32e192a7b5a380063d423ae57
liuchao0206 <liuchao0206@gmail.com>
2017-03-24 00:04:32 +0800

    修改最低版本到 API 14

--- app/src/main/java/com/coderpage/mine/core/Core.java
+++ app/src/main/java/com/coderpage/mine/core/Core.java
@@ -1,29 +1,9 @@
 package com.coderpage.mine.core;
 
-import com.coderpage.framework.concurrency.DefaultWorkExecutor;
-
 /**
  * @author abner-l. 2017-01-22
  */
 
 public class Core {
 
-    private DefaultWorkExecutor mWorkExecutor;
-    private static Core mInstance;
-
-    private Core() {
-        mWorkExecutor = new DefaultWorkExecutor();
-    }
-
-    public static synchronized Core getInstance() {
-        if (mInstance == null) {
-            mInstance = new Core();
-        }
-
-        return mInstance;
-    }
-
-    public DefaultWorkExecutor workExecutor() {
-        return mWorkExecutor;
-    }
 }
